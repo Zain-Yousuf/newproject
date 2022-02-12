9c22ceca03ccc6e9a29d02cc99ff6658bd76df5f
Zain-Yousuf <zainyousuf749@gmail.com>
2022-02-12 18:05:29 +0500

    commit

---
+++ src/components/laptop.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="user">
+     <a href="home">Home</a>
+    <a href="home">login / sign up</a>
+    <a href="home">About us</a>
+    <a href="home">facebook</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "laptop",
+};
+</script>
+
+<style >
+.user {
+  padding: 30px;
+  text-decoration: none;
+  text-align: center;
+  text-justify: center;
+}
+.user a {
+  color: #f2f2f2;
+  margin: 17px;
+  padding: 20px;
+  text-decoration: none;
+  background-color: #333;
+  font-size: 22px;
+  border-radius: 0 10px;
+}
+.user a:hover{
+    box-shadow: 4px 10px #3a003a;
+}
+</style>
